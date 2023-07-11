f6358fa1c293eb10213243bdd9de8bac2ceaebf7
yirleison <yppalomeque@gmail.com>
2023-07-10 22:59:11 -0500

    Se organiza codigo

--- backend/gambatte_db/src/db/connectionDB.js
+++ backend/gambatte_db/src/db/connectionDB.js
@@ -1,14 +1,14 @@
 
-const  Sequelize = require("sequelize") ;
+const Sequelize = require("sequelize");
 
-   let sequelize = new Sequelize(
-        'gambatte_db',
-        'root',
-        '',
-        {
-            host:'localhost',
-            dialect:'mysql'
-        }
-    )
+let sequelize = new Sequelize(
+    'gambatte_db',
+    'root',
+    'root',
+    {
+        host: 'localhost',
+        dialect: 'mysql'
+    }
+)
 
-module.exports ={sequelize};
+module.exports = { sequelize };
