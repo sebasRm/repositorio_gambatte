b52fdba26181b7a25b87b6ada6459174e929a248
sebasRm <71810357+sebasRm@users.noreply.github.com>
2023-07-22 00:56:21 -0500

    update validate image

--- backend/gambatte_db/src/services/uploadServices.js
+++ backend/gambatte_db/src/services/uploadServices.js
@@ -7,51 +7,33 @@ const path = require("path");
 const STATICVAR = require("../helpers/utils").staticVar;
 const { response } = require('../helpers/utils');
 
-
-
-const uploapFile = (req, res) => {
-
-    let img = null
-   let imgTwo=null
-    let keysFiles = Object.keys(req.files)
- if(keysFiles.length>1){
-
-    img = req.files.image
-   
+const validateExtencion = async(img, responseData, res)=>{
+    return new Promise(async (resolve, reject) => {
     let ext_file = ['png', 'PNG', 'jpg', 'JPG', 'jpge', 'jpeg', 'mpge', 'mpg', 'gif'];
     let ext_type = img.name.split('.')[1];
-   
-    imgTwo = req.files.image_two
-    let ext_file_two = ['png', 'PNG', 'jpg', 'JPG', 'jpge', 'jpeg', 'mpge', 'mpg', 'gif'];
-    let ext_type_two = imgTwo.name.split('.')[1];
 
-    if (ext_file.indexOf(ext_type) < 0 || ext_file_two.indexOf(ext_type_two) < 0 ) {
+    if (ext_file.indexOf(ext_type) < 0 ) {
         let msg = STATICVAR.USER_UPDATE_FILE_ERROR.replace('NAME_FILE', img.name)
         msg += msg.replace('EXT_AVALIBLE', ext_file.join(', '))
         return response(msg, 400, res, false, []);
     }
-  
-    else {
+    else{
         let fecha = moment().format('YYYY-MM-DD').replace('-', '_')
         let fileName = `${uuidv4()}-${fecha}-${img.name}`
-        // let ruta = path.resolve(__dirname, '../images')
+        
+        responseData.push(fileName)
         let route = `./src/storage/images/${fileName}`
         img.mv(`${route}`, (error) => {
             if (error) {
                 return response(STATICVAR.USER_UPDATE_AVATAR_ERROR, 400, res, false, []);
             }
-          
-        })
-        let fileNameTwo = `${uuidv4()}-${fecha}-${imgTwo.name}`
-        let routeTwo = `./src/storage/images/${fileNameTwo}`
-        imgTwo.mv(`${routeTwo}`, (error) => {
-            if (error) {
-                return response(STATICVAR.USER_UPDATE_AVATAR_ERROR, 400, res, false, []);
-            }
-            return response(STATICVAR.USER_UPDATE_AVATAR_SUCCESSFULL, 200, res, "ok", fileNameTwo);
-        })
+           return resolve(responseData);
+        }) 
     }
- }else{
+})
+}
+
+const uploapFile = async(req, res) => {
     if (!req.files) {
         return res.status(400).send(
             {
@@ -61,28 +43,23 @@ const uploapFile = (req, res) => {
                 }
             });
     }
-
-    img = req.files.image
-    let ext_file = ['png', 'PNG', 'jpg', 'JPG', 'jpge', 'jpeg', 'mpge', 'mpg', 'gif'];
-    let ext_type = img.name.split('.')[1];
-    if (ext_file.indexOf(ext_type) < 0) {
-        let msg = STATICVAR.USER_UPDATE_FILE_ERROR.replace('NAME_FILE', img.name)
-        msg += msg.replace('EXT_AVALIBLE', ext_file.join(', '))
-        return response(msg, 400, res, false, []);
+    let keysFiles = Object.keys(req.files)
+    let keysValues = Object.values(req.files)
+    let responseData = []
+    if(keysFiles.length>1){
+        for(const keys in keysValues)
+        {
+           let responses=await validateExtencion(keysValues[keys], responseData, res)
+           if(responses.length>1){
+            return response(STATICVAR.USER_UPDATE_AVATAR_SUCCESSFULL, 200, res, "ok", responseData);
+           }
+        }
+    }else{
+        let responses=await validateExtencion(keysValues[0], responseData, res)
+        if(responses.length>0){
+            return response(STATICVAR.USER_UPDATE_AVATAR_SUCCESSFULL, 200, res, "ok", responseData);
+        }
     }
-    else {
-        let fecha = moment().format('YYYY-MM-DD').replace('-', '_')
-        let fileName = `${uuidv4()}-${fecha}-${img.name}`
-        // let ruta = path.resolve(__dirname, '../images')
-        let route = `./src/storage/images/${fileName}`
-        img.mv(`${route}`, (error) => {
-            if (error) {
-                return response(STATICVAR.USER_UPDATE_AVATAR_ERROR, 400, res, false, []);
-            }
-            return response(STATICVAR.USER_UPDATE_AVATAR_SUCCESSFULL, 200, res, "ok", fileName);
-        })
-    }
- }
 }
 
 function getImageFile(req, res) {
